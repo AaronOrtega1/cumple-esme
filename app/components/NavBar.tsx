a8716f66aa3a80c585ef350e90b0bfa8c65adbd9
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-01-29 00:04:43 -0600

    app skeleton, and created navbar component

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-lg font-bold">
+          MyApp
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/gallery" className="text-white hover:text-gray-200">
+              Galería
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
