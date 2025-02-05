14f4a04732c2ca71b1a884ef915b85c14547fe55
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-02-04 22:53:58 -0600

    deleted unused Link import and changed img to Image element

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,15 +1,40 @@
+import Image from "next/image";
+
 export default function Gallery() {
   return (
     <div className="container mx-auto p-4 flex flex-col items-center">
       <h1 className="text-2xl font-bold">Nuestra Galería</h1>
       <div className="grid grid-cols-3 gap-4 mt-4">
-        <img src="/public/MyLove/" alt="1" className="rounded-lg" />
-        <img src="/public/MyLove/" alt="2" className="rounded-lg" />
-        <img src="/public/MyLove/" alt="3" className="rounded-lg" />
-        <img src="/public/MyLove/" alt="4" className="rounded-lg" />
-        <img src="/public/MyLove/" alt="5" className="rounded-lg" />
-        <img src="/public/MyLove/" alt="6" className="rounded-lg" />
+        <Image
+          src="/public/MyLove/"
+          alt="1"
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
+        <Image
+          src="/public/MyLove/"
+          alt="2"
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
+        <Image
+          src="/public/MyLove/"
+          alt="3"
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
+        <Image
+          src="/public/MyLove/"
+          alt="4"
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
       </div>
     </div>
   );
 }
+
