01baa785d759e97cdbd23b866d87d491bc03eb16
AaronOrtega1 <aaron.ortega15@outlook.es>
2025-02-04 22:47:28 -0600

    first prod push

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container flex flex-col items-center min-h-screen py-2">
-      <h1 className="to-blue-500">
-        Hola mi amor
-      </h1>
+      <h1 className="to-blue-500">Hola mi amor</h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
   );
